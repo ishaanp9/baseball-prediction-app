a484a2384f07e06a966ef7ba2f84fef1338da0e6
Ishaan Puri <ishaanpuri9@gmail.com>
2022-07-14 19:51:34 -0700

    sc[25] made player info banner

---
+++ ui/src/Pages/PlayerPage/PlayerPage.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const PlayerInformationBannerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: black;
+  color: white;
+  display: flex;
+  margin: 5%;
+  border-radius: 16px;
+`;
+
+export const PlayerInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PlayerStockStatsContianer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const PlayerStockStatContianer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PlayerStockStatText = styled.p`
+  color: white;
+  font-size: 25px;
+`;
+
+export const PlayerTeamPositionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const PlayerName = styled.p`
+  color: white;
+  font-size: 25px;
+`;
+
+export const PlayerTeam = styled.p`
+  color: white;
+  font-size: 25px;
+`;
+
+export const PlayerPosition = styled.p`
+  color: white;
+  font-size: 25px;
+`;
